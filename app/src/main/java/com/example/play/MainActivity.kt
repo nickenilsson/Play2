73dc264504e168b67b95fd529c569374349d0420
Hannes Hedell <hanneshedell@MacBook-Pro-3.local>
2022-01-10 16:53:20 +0100

    set up view binding in main activity

--- app/src/main/java/com/example/play/MainActivity.kt
+++ app/src/main/java/com/example/play/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.play
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.play.databinding.ActivityMainBinding
+
+interface MainActivityViewModel {
+    val title: String
+    val subtitle: String
+}
+
+class MockedMainActivityViewModel: MainActivityViewModel {
+    override val title: String = "TestTitle"
+    override val subtitle: String = "TestSubtitle"
+}
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.textViewTitle.setText(viewModel.title)
+
+        binding.textViewSubtitle.setText(viewModel.subtitle)
+
 
     }
 }
